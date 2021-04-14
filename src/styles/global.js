cb4db5a5b319ea613c92cde17db2f00401e30bb8
Adam Collier <adamcollierart@gmail.com>
2021-04-14 10:06:31 +0100

    feature: source global styled from single file

---
+++ src/styles/global.js
@@ -0,0 +1,39 @@
+export const globalStyles = `
+    :root{
+      --primary-black: #141414;
+      --primary-white: #FFFFFF;
+      --border-radius: 3px;
+    }
+    
+    *,
+    :after,
+    :before {
+        box-sizing: border-box;
+    }
+
+    .breadcrumbs {
+        display: none;
+    }
+
+    @media (min-width: 768px){
+        .main-container {
+        max-width: none;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: 0;
+        padding-right: 0;
+        }
+    }
+
+    p {
+        font-family: 'Helvetica Neue', Arial, sans-serif;
+        line-height: 1.75;
+        font-size: 1rem;
+        font-weight: 400;
+        margin: 0;
+    }
+
+    body {
+        margin: 0;
+    }
+`;
