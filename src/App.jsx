71f81b0c0c84dbfb8667ab58f80e9a9431f37273
Adam Collier <adamcollierart@gmail.com>
2021-11-05 12:54:33 +0000

    fix: amendments

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { createGlobalStyle } from "styled-components";
 import { globalStyles } from "./styles/global";
-import KlarnaHaulidayLogo from "./svgs/hermes.svg?component";
+import KlarnaLogo from "./svgs/klarna.svg?component";
 import { Button, Image, Flex, Stack, Text } from "vite-storybook";
 import styled from "styled-components";
 
@@ -24,9 +24,34 @@ const GlobalStyle = createGlobalStyle`
 // };
 
 const HeroTwoCol = styled(Flex)`
+  background: #fdafc7;
+
   > section:first-of-type {
     padding: 2rem;
   }
+
+  @media (min-width: 1281px) {
+    border-radius: 5px;
+    overflow: hidden;
+
+    > section:first-of-type {
+      padding: 4rem;
+    }
+  }
+`;
+
+const HeroImage = styled(Image)`
+  align-self: stretch;
+`;
+
+const Link = styled.a`
+  color: var(--primary-black);
+  text-decoration: underline;
+`;
+
+const OrderedList = styled.ol`
+  list-style-type: revert;
+  padding-left: revert;
 `;
 
 const Container = styled(Stack)`
@@ -41,43 +66,96 @@ function App() {
       <GlobalStyle />
       {/* the container className is important for the static lazyloading script */}
       <Container gap={3} className="container">
-        <HeroTwoCol itemFlex="1 1 450px" align="center" maxWidth="xl">
+        <HeroTwoCol itemFlex="1 1 450px" align="center" maxWidth="xl" gap={0}>
           <Flex align="center" justify="center">
             <Flex gap={1} direction="column" customMaxWidth={400}>
-              <div style={{ width: "160px" }}>
-                <KlarnaHaulidayLogo />
+              <div style={{ width: "120px" }}>
+                <KlarnaLogo />
               </div>
-              <Text size="2xl" titling>
-                Hermes Play with Missguided
+              <Text size="xl" titling>
+                Win a weekend break for you and a friend with Klarna
               </Text>
-              <Text size="lg">
-                Dress for the weather you want and score 25% off your next order
-                using code HERMES25*
+              <Text size="md">
+                The prize includes a £650 Hotels.com voucher as well as a £350
+                as bank transfer to cover your travel and food expenses.
               </Text>
-              <Button text="shop now" link="/new-in" />
+              <Text>To enter all you have to do is…</Text>
+              <OrderedList>
+                <Text element="li">
+                  Follow the official{" "}
+                  <Link
+                    href="https://www.instagram.com/klarna.uk/"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    @klarna.uk
+                  </Link>{" "}
+                  and{" "}
+                  <Link
+                    href="https://www.instagram.com/missguided/"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    @Missguided
+                  </Link>{" "}
+                  Instagram profiles
+                </Text>
+                <Text element="li">
+                  <Text>Like + Save the mural post</Text>
+                </Text>
+                <Text element="li">
+                  <Text>Tag the friend you’d take with you</Text>
+                </Text>
+              </OrderedList>
+              <Text size="md">
+                The Winner will be contacted via direct message. Best of luck!
+              </Text>
+              <Button text="TAKE ME TO THE INSTA POST" link="/new-in" />
             </Flex>
           </Flex>
-          <Image
+          <HeroImage
             alt="hero image"
             height={640}
             sizes="(max-width: 600px) 100vw, (min-width: 1280px) 632px, 50vw"
-            src="https://media.missguided.com/i/missguided/hermes_hero"
+            src="https://media.missguided.com/i/missguided/klarna_mural_hero"
             width={640}
             quality={90}
           />
         </HeroTwoCol>
         <Stack gap={1.45} maxWidth="lg" padding>
           <Text size="xs" align="center" color="grey-3" weight={400}>
-            *25% off Everything Code: HERMES25. This offer for a valid from the
-            29th June 2021 to 6th July 2021 and entitles customers to 25%
-            discount on their order. Offer applies to items purchased from the
-            www.missguided.co.uk website and Missguided UK App, and excludes
-            sale, Restyld, homeware, Playboy, outlet and beauty. This offer
-            cannot be used in conjunction with any other code, discount, offer
-            or promotion. To redeem enter code HERMES25 at the checkout. This
-            promotion is administered by Missguided Limited (company number
-            3235904) of Wharfside, 75 Trafford Wharf Road, Salford, Manchester,
-            M17 1ES.
+            Prize – One (1) winner will be randomly selected (Prize Winner”) and
+            the prize winner will receive the following (“Prize”): * A £650
+            Hotels.com voucher * £350 as bank transfer to cover your travel and
+            food expenses
+          </Text>
+          <Text size="xs" align="center" color="grey-3" weight={400}>
+            Winner Selection – The Prize Winner will need to share details for a
+            bank transfer and an email address to be able to claim the prize.
+            The Prize, or any portion of the Prize, is not transferable or
+            exchangeable and cannot be redeemed for cash. No compensation will
+            be offered if the Prize Winner is unable to accept the Prize. The
+            Prize Winner is responsible for any tax considerations related to
+            accepting the Prize and should seek independent financial advice
+            prior to acceptance of the Prize. Additional costs resulting from
+            acceptance of the Prize are the responsibility of the Prize Winner.
+            The Promoter reserves the right to substitute any portion of the
+            Prize for an alternative of equal or greater value.
+          </Text>
+          <Text size="xs" align="center" color="grey-3" weight={400}>
+            With whom does the Promoter share your personal data? The Promoter
+            may share your personal data with suppliers or subcontractors it
+            uses in order to provide the Promotion to you. Suppliers and
+            subcontractors are companies who are only entitled to process the
+            personal data they receive from the Promoter based on the
+            instructions of the Promoter. Categories of such suppliers and
+            subcontractors are software and data storage providers and business
+            consultants. Your personal data may also be shared with third
+            parties including Hotels.com so that the Prize can be provided to
+            the Prize Winner. The basis for sharing the information with these
+            third parties is to fulfill the Promoter’s contractual obligations
+            under these Terms and Conditions in order to provide the Prize to
+            the Prize Winner.
           </Text>
         </Stack>
       </Container>
