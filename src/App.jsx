22f2b5ee2359583c8f884cd4309185714c2893c4
Adam Collier <adamcollierart@gmail.com>
2021-11-09 14:38:45 +0000

    chore: adjust terms and link

--- src/App.jsx
+++ src/App.jsx
@@ -108,9 +108,10 @@ function App() {
                 </Text>
               </OrderedList>
               <Text size="md">
-                The Winner will be contacted via direct message. Best of luck!
+                Look for the image post of the <strong>mural</strong> on our
+                <strong> feed</strong>. The Winner will be contacted via direct
+                message. Best of luck!
               </Text>
-              <Button text="TAKE ME TO THE INSTA POST" link="/new-in" />
             </Flex>
           </Flex>
           <HeroImage
@@ -124,31 +125,34 @@ function App() {
         </HeroTwoCol>
         <Stack gap={1.45} maxWidth="lg" padding>
           <Text size="xs" align="center" color="grey-3" weight={400}>
-            Prize – One (1) winner will be randomly selected (Prize Winner”) and
-            the prize winner will receive the following (“Prize”): * A £650
-            Hotels.com voucher * £350 as bank transfer to cover your travel and
-            food expenses
+            <strong>Prize</strong> – One (1) winner will be randomly selected
+            (Prize Winner”) and the prize winner will receive the following
+            (“Prize”): * A £650 Hotels.com voucher * £350 as bank transfer to
+            cover your travel and food expenses
           </Text>
           <Text size="xs" align="center" color="grey-3" weight={400}>
-            Winner Selection – The Prize Winner will need to share details for a
-            bank transfer and an email address to be able to claim the prize.
-            The Prize, or any portion of the Prize, is not transferable or
-            exchangeable and cannot be redeemed for cash. No compensation will
-            be offered if the Prize Winner is unable to accept the Prize. The
-            Prize Winner is responsible for any tax considerations related to
-            accepting the Prize and should seek independent financial advice
-            prior to acceptance of the Prize. Additional costs resulting from
-            acceptance of the Prize are the responsibility of the Prize Winner.
-            The Promoter reserves the right to substitute any portion of the
-            Prize for an alternative of equal or greater value.
+            <strong>Winner Selection</strong> – The Prize Winner will need to
+            share details for a bank transfer and an email address to be able to
+            claim the prize. The Prize, or any portion of the Prize, is not
+            transferable or exchangeable and cannot be redeemed for cash. No
+            compensation will be offered if the Prize Winner is unable to accept
+            the Prize. The Prize Winner is responsible for any tax
+            considerations related to accepting the Prize and should seek
+            independent financial advice prior to acceptance of the Prize.
+            Additional costs resulting from acceptance of the Prize are the
+            responsibility of the Prize Winner. The Promoter reserves the right
+            to substitute any portion of the Prize for an alternative of equal
+            or greater value.
           </Text>
           <Text size="xs" align="center" color="grey-3" weight={400}>
-            With whom does the Promoter share your personal data? The Promoter
-            may share your personal data with suppliers or subcontractors it
-            uses in order to provide the Promotion to you. Suppliers and
-            subcontractors are companies who are only entitled to process the
-            personal data they receive from the Promoter based on the
-            instructions of the Promoter. Categories of such suppliers and
+            <strong>
+              With whom does the Promoter share your personal data?
+            </strong>{" "}
+            - The Promoter may share your personal data with suppliers or
+            subcontractors it uses in order to provide the Promotion to you.
+            Suppliers and subcontractors are companies who are only entitled to
+            process the personal data they receive from the Promoter based on
+            the instructions of the Promoter. Categories of such suppliers and
             subcontractors are software and data storage providers and business
             consultants. Your personal data may also be shared with third
             parties including Hotels.com so that the Prize can be provided to
@@ -156,6 +160,17 @@ function App() {
             third parties is to fulfill the Promoter’s contractual obligations
             under these Terms and Conditions in order to provide the Prize to
             the Prize Winner.
+          </Text>
+          <Text size="xs" align="center" color="grey-3" weight={400}>
+            Missguided is not a lender and acts only as an introducer. Please
+            spend responsibly. Borrowing beyond your means could seriously
+            affect your financial status. Klarna’s Pay in 3 instalments and Pay
+            in 30 days credit agreement[s] are not regulated by the FCA. 18+, UK
+            residents only. No interest, no fees. Credit subject to status. T&Cs
+            apply.{" "}
+            <Link href="https://klarna.com/uk/terms-and-conditions">
+              klarna.com/uk/terms-and-conditions.
+            </Link>
           </Text>
         </Stack>
       </Container>
